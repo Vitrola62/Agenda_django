c6bd3a86ae12f10f0d7003b2cdb43229d2d937d0
Vitor Dias <vitordias-sousa@hotmail.com>
2023-04-07 16:31:43 -0300

    .

--- contatos/views.py
+++ contatos/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from .models import Contato
+from django.core.paginator import Paginator
 
 def index(request):
-    return render(request, 'contatos/index.html')
+    contatos = Contato.objects.all()
+    paginator = Paginator(contatos, 10)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(p)
+    return render(request, 'contatos/index.html', {
+        'contatos': contatos
+    })
+
+def ver_contato(request, contato_id):
+    contato = get_object_or_404(Contato, id=contato_id)
+    return render(request, 'contatos/ver_contato.html', {
+        'contato': contato
+    })
